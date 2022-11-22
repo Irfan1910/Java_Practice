b4b703683973a4d1fd673449d43b6b6cdea71ade
Irfan1910 <118262460+Irfan1910@users.noreply.github.com>
2022-11-22 14:45:17 +0530

    Add files via upload

---
+++ My profile.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author PGSYS
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("My Profile");
+        System.out.println("Name: Peer Mohamed Irfan");
+        System.out.println("MObile:9003671624");
+        System.out.println("E-mail:pmirfan1910@gmail.com");
+        // TODO code application logic here
+    }
+    
+}
