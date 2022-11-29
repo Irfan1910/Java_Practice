f476112aeb669074a602de91627814a4cee1b691
Peer_Mohamed_Irfan <118262460+PM-Irfan@users.noreply.github.com>
2022-11-29 15:25:13 +0530

    Add files via upload

---
+++ VariableString.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variable.string;
+
+/**
+ *
+ * @author PGSYSTEM
+ */
+public class VariableString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name="Peer Mohamed Irfan";
+                System.out.println(name);
+        // TODO code application logic here
+    }
+    
+}
