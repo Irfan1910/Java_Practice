06d156e02d513a8ff2cf0d52d03818389f5a277f
Peer_Mohamed_Irfan <118262460+PM-Irfan@users.noreply.github.com>
2022-11-29 14:39:26 +0530

    Add files via upload

---
+++ MyProfile.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.profile;
+
+/**
+ *
+ * @author PGSYSTEM
+ */
+public class MyProfile {
+     
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("My Profile");
+        System.out.println("Name:Peer Mohamed Irfan");
+        System.out.println("Qualification:B.E. mechanical");
+        System.out.println("Mobile:9003671624");
+        System.out.println("Email:pmirfan1910@gmail.com");
+        System.out.println("Date_of_birth:19 october");
+        System.out.println("Languages known:English, Tamil, Malayalam");
+        
+        // TODO code application logic here
+    }
+    
+}
