4e1dc43645feb2866d0bdbddf47529c100f5c8b1
Peer_Mohamed_Irfan <118262460+PM-Irfan@users.noreply.github.com>
2022-11-29 15:48:44 +0530

    Add files via upload

---
+++ AdditionOf2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addition.of.pkg2;
+
+/**
+ *
+ * @author PGSYSTEM
+ */
+public class AdditionOf2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int number1=500;
+        int number2=600;
+        int sum=number1+number2;
+        System.out.println("The sum is "+sum);
+        // TODO code application logic here
+    }
+    
+}
