b92c558608d0238e05147ac934701eb7cb79c539
Peer_Mohamed_Irfan <118262460+PM-Irfan@users.noreply.github.com>
2022-11-29 15:35:03 +0530

    Add files via upload

---
+++ VariableIntegerchangenum.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author PGSYSTEM
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num=90;
+        num=91;//num is now 91
+        System.out.println(num);
+        // TODO code application logic here
+    }
+        // TODO code application logic here
+    }
+    
+}
