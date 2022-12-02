2bb936ede423d5cac2c19adc2fed202fc8a927bf
Peer_Mohamed_Irfan <118262460+pmirfan@users.noreply.github.com>
2022-12-02 15:22:14 +0530

    Add files via upload

---
+++ bank/src/bank/Bank.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+/**
+ *
+ * @author PG LAB
+ */
+public class Bank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        bank_1 icici=new bank_1();
+        icici.bank_name= "ICICI Bank";
+        icici.display_interest();
+        
+        
+    }
+
+    private static void display_interest() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
---
+++ bank/src/bank/bank_1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+/**
+ *
+ * @author PG LAB
+ */
+class bank_1 {
+    String bank_name= "ICICI";
+    int interest= 7;
+    void display_interest()
+    {
+        System.out.println("The interest in the " + bank_name + "interest is " + interest);
+    }    
+}
